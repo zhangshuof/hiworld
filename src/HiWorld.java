4b63495cebc1a0a91215b6bb90ba2b9f1addaff0
zhangshuof <867030396@qq.com>
2018-07-02 16:06:11 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,5 +1,16 @@
 public class HiWorld {
     public static void main(String args[]){
         System.out.println("Hi World !!!!");
+        System.out.println(aaa());
+    }
+
+    public static int aaa(){
+        try{
+            return 1;
+        }catch(Exception e){
+            return 2;
+        }finally {
+            return 3;
+        }
     }
 }
